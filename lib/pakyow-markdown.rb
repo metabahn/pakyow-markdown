c81a3cad94247999764f1e8a5df7815aeb2109cb
Bryan Powell <bryan@metabahn.com>
2015-07-25 23:34:23 -0500

    Silence warnings

--- lib/pakyow-markdown.rb
+++ lib/pakyow-markdown.rb
@@ -1,16 +1,18 @@
 libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
-# Gems
 require 'redcarpet'
-require 'rouge'
-require 'rouge/plugins/redcarpet'
 
+# silences the annoying redefined constant warnings
+Kernel.silence_warnings do
+  require 'rouge'
+end
+
+require 'rouge/plugins/redcarpet'
 class Renderer < Redcarpet::Render::HTML
   include Rouge::Plugins::Redcarpet
 end
 
-# Config
 Pakyow::Config.register(:markdown) { |config|
   # extensions to load with redcarpet
   config.opt :extensions, {
@@ -20,7 +22,6 @@ Pakyow::Config.register(:markdown) { |config|
   }
 }
 
-# Processor
 Pakyow::App.processor(:md, :mdown, :markdown) do |content|
   markdown = Redcarpet::Markdown.new(Renderer, **Pakyow::Config.markdown.extensions)
   markdown.render(content)
